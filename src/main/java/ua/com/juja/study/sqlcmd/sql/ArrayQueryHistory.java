62274a9025a39a1b3505a54a9956cb5173d7d826
vviictoorr <victor.pidleteychuk@gmail.com>
2014-11-10 10:47:26 +0200

    ArrayQueryHistory was changed

--- src/main/java/ua/com/juja/study/sqlcmd/sql/ArrayQueryHistory.java
+++ src/main/java/ua/com/juja/study/sqlcmd/sql/ArrayQueryHistory.java
@@ -4,34 +4,33 @@ package ua.com.juja.study.sqlcmd.sql;
  * Created by VICTOR on 31.10.2014.
  */
 public class ArrayQueryHistory implements QueryHistory {
-    private static final int NO_QUARY_IN_ARRAY = -1;
-    private int indexQuary = NO_QUARY_IN_ARRAY;
+    private static final int NO_QUERY_IN_ARRAY = -1;
+    private int indexQuery = NO_QUERY_IN_ARRAY;
     private static String[] queryCount = new String[QUERY_BUFFER_COUNT];
 
     @Override
     public String getNextQuery() {
-        if (indexQuary < 0) {
-            indexQuary = QUERY_BUFFER_COUNT - 1;
-            return queryCount[indexQuary--];
+        if (queryCount.length != 0 && indexQuery >= 0) {
+            return queryCount[indexQuery--];
         }
-        return queryCount[indexQuary--];
+        return null;
     }
 
     @Override
     public String getPreviousQuery() {
-        if (indexQuary == QUERY_BUFFER_COUNT - 2) {
-            indexQuary = 0;
-            return queryCount[indexQuary++];
+        if (queryCount.length != 0 && indexQuery < QUERY_BUFFER_COUNT) {
+            return queryCount[++indexQuery];
         }
-        return queryCount[indexQuary++];
+        return null;
     }
 
     @Override
     public void addQueryToTheHead(String query) {
-        if (indexQuary < QUERY_BUFFER_COUNT - 1) {
-            indexQuary++;
-            queryCount[indexQuary] = query;
+        if (indexQuery < QUERY_BUFFER_COUNT - 1) {
+            queryCount[++indexQuery] = query;
         } else {
+            System.arraycopy(queryCount, 0, queryCount, 1,
+                    QUERY_BUFFER_COUNT - 1);
             queryCount[0] = query;
         }
     }
