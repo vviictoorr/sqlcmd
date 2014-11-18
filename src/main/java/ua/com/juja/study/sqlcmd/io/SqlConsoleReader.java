e79e7423c632d02f52be0bb986a07104b3d7da0f
vviictoorr <victor.pidleteychuk@gmail.com>
2014-11-18 16:26:53 +0200

    synchronized SqlConsoleReader

---
+++ src/main/java/ua/com/juja/study/sqlcmd/io/SqlConsoleReader.java
@@ -0,0 +1,94 @@
+package ua.com.juja.study.sqlcmd.io;
+
+import jline.ConsoleReader;
+import jline.CursorBuffer;
+
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viktor
+ * Date: 11/17/14
+ * Time: 9:42 PM
+ */
+public class SqlConsoleReader {
+
+    public static final int SEMICOLON = 59;
+    public static final int NEW_LINE = 10;
+    public static final int BACKSPACE = 127;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 6;
+    public static final int END = 5;
+    public static final int HOME = 1;
+    private ConsoleReader consoleReader;
+    private Writer writer;
+    private CursorBuffer cursorBuffer;
+
+    public static void main(String[] args) {
+
+    }
+
+    public SqlConsoleReader(InputStream inputStream, OutputStream out) throws IOException {
+        writer = new PrintWriter(out);
+        consoleReader = new ConsoleReader(inputStream, writer);
+        cursorBuffer = consoleReader.getCursorBuffer();
+    }
+
+    public String readQuery() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        boolean exit = false;
+        int previousKey = 0;
+        while (!exit) {
+            int key = consoleReader.readVirtualKey();
+            switch (key) {
+                case NEW_LINE:
+                    sb.append(cursorBuffer.toString());
+                    if (previousKey == SEMICOLON) {
+                        exit = true;
+                    } else {
+                        sb.append(" ");
+                        newLine();
+                    }
+                    break;
+                case BACKSPACE:
+                    consoleReader.backspace();
+                    break;
+                case LEFT:
+                    consoleReader.moveCursor(-1);
+                    break;
+                case RIGHT:
+                    consoleReader.moveCursor(1);
+                    break;
+                case END:
+                    moveToEnd();
+                    break;
+                case HOME:
+                    moveToBegin();
+                    break;
+                case 98:
+
+                default:
+                    cursorBuffer.write((char) key);
+                    writer.write(key);
+            }
+            previousKey = key;
+            consoleReader.flushConsole();
+        }
+        return sb.toString();
+    }
+
+    private void moveToBegin() throws IOException {
+        consoleReader.moveCursor(-cursorBuffer.length());
+    }
+
+    private void moveToEnd() throws IOException {
+        consoleReader.moveCursor(cursorBuffer.length() - cursorBuffer.cursor);
+    }
+
+    private void newLine() throws IOException {
+        consoleReader.moveCursor(0);
+        consoleReader.printNewline();
+        cursorBuffer.clearBuffer();
+        cursorBuffer.cursor = 0;
+    }
+}
