d3535566b70e4751feeeac9db91900d1c40d25d6
VICTOR <victor.pidleteychuk@gmail.com>
2015-08-09 15:24:31 +0300

    fixed writer

--- src/main/java/ua/com/juja/study/sqlcmd/database/jdbc/JdbcDatabaseExecutor.java
+++ src/main/java/ua/com/juja/study/sqlcmd/database/jdbc/JdbcDatabaseExecutor.java
@@ -19,6 +19,34 @@ import static ua.com.juja.study.sqlcmd.database.Row.ROWS_AFFECTED;
  * Time: 12:05 PM
  */
 public class JdbcDatabaseExecutor implements DatabaseExecutor {
+    public static void main(String[] args) throws DatabaseException, SQLException {
+//        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/juja");
+
+        SqlCmdConfig config = new SqlCmdConfig();
+        config.setDbUrl("jdbc:postgresql://localhost:5432/juja");
+        config.setDriverName("org.postgresql.Driver");
+        config.setUserName("juja_core");
+        config.setPassword("juja");
+        JdbcDatabaseExecutor executor = new JdbcDatabaseExecutor(config);
+
+        QueryResult result = executor.executeSqlScript("select * from participations");
+        printResult(result);
+    }
+
+    private static void printResult(QueryResult result) throws DatabaseException {
+        String[] columns = result.getColumnNames();
+        for (String column : columns) {
+            System.out.print(column + " | ");
+        }
+        System.out.println();
+        Row[] rows = result.getRowList();
+        for (Row row : rows) {
+            for (String column : columns) {
+                System.out.print(row.getValue(column) + " | ");
+            }
+            System.out.println();
+        }
+    }
 
     private Connection connection;
 
--- src/main/java/ua/com/juja/study/sqlcmd/io/ConsoleFormattedQueryResultWriter.java
+++ src/main/java/ua/com/juja/study/sqlcmd/io/ConsoleFormattedQueryResultWriter.java
@@ -1,7 +1,9 @@
 package ua.com.juja.study.sqlcmd.io;
 
 import ua.com.juja.study.sqlcmd.database.QueryResult;
+import ua.com.juja.study.sqlcmd.database.Row;
 import ua.com.juja.study.sqlcmd.di.ApplicationContext;
+import ua.com.juja.study.sqlcmd.di.DefaultApplicationContext;
 import ua.com.juja.study.sqlcmd.engine.QueryFormatter;
 
 import java.io.BufferedWriter;
@@ -10,6 +12,8 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by VICTOR on 03.12.2014.
@@ -55,5 +59,33 @@ public class ConsoleFormattedQueryResultWriter implements ResultWriter {
 
     public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ConsoleFormattedQueryResultWriter consoleWriter = new ConsoleFormattedQueryResultWriter();
+        ApplicationContext applicationContext = new DefaultApplicationContext(null);
+        consoleWriter.setApplicationContext(applicationContext);
+
+        FutureTask<Row[]> task = new FutureTask(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                Thread.sleep(20000);
+                return new Row[]{};
+            }
+        });
+        QueryResult queryResult = new QueryResult(task);
+        consoleWriter.writeQueryResult(queryResult);
+        System.out.println("WriteQueryResult");
+        applicationContext.getExecutorService().submit(task);
+
+        while (!task.isDone()) {
+            Row[] rows = new Row[0];
+            try {
+                rows = task.get(100L, TimeUnit.MILLISECONDS);
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        applicationContext.shutdown();
     }
 }
